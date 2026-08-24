5e6d13f33348d9e8f1ed125c4daa4542f2aa623a
Sanjana Bhat <sanjanabhat2103@gmail.com>
2026-08-25 00:09:30 +0530

    Step 11: he_init

--- model.py
+++ model.py
@@ -63,8 +63,11 @@ def accuracy(logits_or_probs, labels):
 def he_std(fan_in):
     return np.sqrt(2 / fan_in)
 
-# Step 11 - he_init (not yet solved)
-# TODO: implement
+# Step 11 - he_init
+def he_init(shape, fan_in, seed):
+    std = he_std(fan_in)
+    rng = np.random.RandomState(seed)
+    return rng.normal(loc = 0.0, scale = std, size = shape)
 
 # Step 12 - init_zero_bias (not yet solved)
 # TODO: implement
